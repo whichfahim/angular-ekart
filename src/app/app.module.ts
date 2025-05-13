d418d5f312dd7a864847765f003d9ee2ecb84c99
Fahim Imtiaz <fahim.imtiaz07@gmail.com>
2025-05-13 19:39:01 +0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration(withEventReplay())],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: false,
+  template: '<h2>eKart</h2>',
+})
+export class HeaderComponent {}
